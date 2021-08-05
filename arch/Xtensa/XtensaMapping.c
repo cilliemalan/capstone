19685d02de927fd5e3e17345165eeaa2277b89e8
Cillié Malan <cilliemalan@gmail.com>
2021-08-05 11:21:28 +0200

    stuff now lowercase to match gcc instead of ref

--- arch/Xtensa/XtensaMapping.c
+++ arch/Xtensa/XtensaMapping.c
@@ -44,14 +44,15 @@ static const char *xtensa_registers[] =
 static const char *xtensa_instructions[] =
 	{
 		NULL,
-		"ABS",
-		"ABS_S",
-		"ADD",
-		"ADD_N",
-		"ADD_S",
-		"ADDI",
-		"MOVI",
-		"NEG",
+		"abs",
+		"abs.s",
+		"add",
+		"add.n",
+		"add.s",
+		"addi",
+		"movi",
+		"movi.n",
+		"neg",
 };
 
 static const char *xtensa_groups[] =
@@ -79,7 +80,7 @@ static const char *xtensa_groups[] =
 
 void Xtensa_get_insn_id(cs_struct *h, cs_insn *insn, unsigned int id)
 {
-	insn->id = insn->id;
+	insn->id = id;
 }
 
 const char *Xtensa_reg_name(csh handle, unsigned int id)
@@ -98,160 +99,83 @@ const char *Xtensa_sysreg_name(csh handle, unsigned int id)
 {
 	switch (id)
 	{
-	case XTENSA_SPECIAL_REG_LBEG:
-		return "LBEG";
-	case XTENSA_SPECIAL_REG_LEND:
-		return "LEND";
-	case XTENSA_SPECIAL_REG_LCOUNT:
-		return "LCOUNT";
-	case XTENSA_SPECIAL_REG_SAR:
-		return "SAR";
-	case XTENSA_SPECIAL_REG_BR:
-		return "BR";
-	case XTENSA_SPECIAL_REG_LITBASE:
-		return "LITBASE";
-	case XTENSA_SPECIAL_REG_SCOMPARE1:
-		return "SCOMPARE1";
-	case XTENSA_SPECIAL_REG_ACCLO:
-		return "ACCLO";
-	case XTENSA_SPECIAL_REG_ACCHI:
-		return "ACCHI";
-	case XTENSA_SPECIAL_REG_MR0:
-		return "MR0";
-	case XTENSA_SPECIAL_REG_MR1:
-		return "MR1";
-	case XTENSA_SPECIAL_REG_MR2:
-		return "MR2";
-	case XTENSA_SPECIAL_REG_MR3:
-		return "MR3";
-	case XTENSA_SPECIAL_REG_WINDOWBASE:
-		return "WindowBase";
-	case XTENSA_SPECIAL_REG_WINDOWSTART:
-		return "WindowStart";
-	case XTENSA_SPECIAL_REG_PTEVADDR:
-		return "PTEVADDR";
-	case XTENSA_SPECIAL_REG_MMID:
-		return "MMID";
-	case XTENSA_SPECIAL_REG_RASID:
-		return "RASID";
-	case XTENSA_SPECIAL_REG_ITLBCFG:
-		return "ITLBCFG";
-	case XTENSA_SPECIAL_REG_DTLBCFG:
-		return "DTLBCFG";
-	case XTENSA_SPECIAL_REG_IBREAKENABLE:
-		return "IBREAKENABLE";
-	case XTENSA_SPECIAL_REG_ATOMCTL:
-		return "ATOMCTL";
-	case XTENSA_SPECIAL_REG_DDR:
-		return "DDR";
-	case XTENSA_SPECIAL_REG_MEPS:
-		return "MEPS";
-	case XTENSA_SPECIAL_REG_MEPC:
-		return "MEPC";
-	case XTENSA_SPECIAL_REG_MESAVE:
-		return "MESAVE";
-	case XTENSA_SPECIAL_REG_MESR:
-		return "MESR";
-	case XTENSA_SPECIAL_REG_MECR:
-		return "MECR";
-	case XTENSA_SPECIAL_REG_MEVADDR:
-		return "MEVADDR";
-	case XTENSA_SPECIAL_REG_IBREAKA0:
-		return "IBREAKA0";
-	case XTENSA_SPECIAL_REG_IBREAKA1:
-		return "IBREAKA1";
-	case XTENSA_SPECIAL_REG_DEBUGCAUSE:
-		return "DEBUGCAUSE";
-	case XTENSA_SPECIAL_REG_DBREAKA0:
-		return "DBREAKA0";
-	case XTENSA_SPECIAL_REG_DBREAKA1:
-		return "DBREAKA1";
-	case XTENSA_SPECIAL_REG_DBREAKC0:
-		return "DBREAKC0";
-	case XTENSA_SPECIAL_REG_DBREAKC1:
-		return "DBREAKC1";
-	case XTENSA_SPECIAL_REG_EPC1:
-		return "EPC1";
-	case XTENSA_SPECIAL_REG_EPC2:
-		return "EPC2";
-	case XTENSA_SPECIAL_REG_EPC3:
-		return "EPC3";
-	case XTENSA_SPECIAL_REG_EPC4:
-		return "EPC4";
-	case XTENSA_SPECIAL_REG_EPC5:
-		return "EPC5";
-	case XTENSA_SPECIAL_REG_EPC6:
-		return "EPC6";
-	case XTENSA_SPECIAL_REG_EPC7:
-		return "EPC7";
-	case XTENSA_SPECIAL_REG_DEPC:
-		return "DEPC";
-	case XTENSA_SPECIAL_REG_EPS2:
-		return "EPS2";
-	case XTENSA_SPECIAL_REG_EPS3:
-		return "EPS3";
-	case XTENSA_SPECIAL_REG_EPS4:
-		return "EPS4";
-	case XTENSA_SPECIAL_REG_EPS5:
-		return "EPS5";
-	case XTENSA_SPECIAL_REG_EPS6:
-		return "EPS6";
-	case XTENSA_SPECIAL_REG_EPS7:
-		return "EPS7";
-	// TODO :shrug: why is EXCSAVE1 == DEPC ??
-	// case XTENSA_SPECIAL_REG_EXCSAVE1: return "EXCSAVE1";
-	case XTENSA_SPECIAL_REG_EXCSAVE2:
-		return "EXCSAVE2";
-	case XTENSA_SPECIAL_REG_EXCSAVE3:
-		return "EXCSAVE3";
-	case XTENSA_SPECIAL_REG_EXCSAVE4:
-		return "EXCSAVE4";
-	case XTENSA_SPECIAL_REG_EXCSAVE5:
-		return "EXCSAVE5";
-	case XTENSA_SPECIAL_REG_EXCSAVE6:
-		return "EXCSAVE6";
-	case XTENSA_SPECIAL_REG_EXCSAVE7:
-		return "EXCSAVE7";
-	case XTENSA_SPECIAL_REG_CPENABLE:
-		return "CPENABLE";
-	case XTENSA_SPECIAL_REG_INTERRUPT:
-		return "INTERRUPT";
-	case XTENSA_SPECIAL_REG_INTCLEAR:
-		return "INTCLEAR";
-	case XTENSA_SPECIAL_REG_INTENABLE:
-		return "INTENABLE";
-	case XTENSA_SPECIAL_REG_PS:
-		return "PS";
-	case XTENSA_SPECIAL_REG_VECBASE:
-		return "VECBASE";
-	case XTENSA_SPECIAL_REG_EXCCAUSE:
-		return "EXCCAUSE";
-	case XTENSA_SPECIAL_REG_CCOUNT:
-		return "CCOUNT";
-	case XTENSA_SPECIAL_REG_PRID:
-		return "PRID";
-	case XTENSA_SPECIAL_REG_ICOUNT:
-		return "ICOUNT";
-	case XTENSA_SPECIAL_REG_ICOUNTLEVEL:
-		return "ICOUNTLEVEL";
-	case XTENSA_SPECIAL_REG_EXCVADDR:
-		return "EXCVADDR";
-	case XTENSA_SPECIAL_REG_CCOMPARE0:
-		return "CCOMPARE0";
-	case XTENSA_SPECIAL_REG_CCOMPARE1:
-		return "CCOMPARE1";
-	case XTENSA_SPECIAL_REG_CCOMPARE2:
-		return "CCOMPARE2";
-	case XTENSA_SPECIAL_REG_MISC0:
-		return "MISC0";
-	case XTENSA_SPECIAL_REG_MISC1:
-		return "MISC1";
-	case XTENSA_SPECIAL_REG_MISC2:
-		return "MISC2";
-	case XTENSA_SPECIAL_REG_MISC3:
-		return "MISC3";
-	default:
-		return NULL;
+	case XTENSA_SPECIAL_REG_LBEG: return "lbeg";
+	case XTENSA_SPECIAL_REG_LEND: return "lend";
+	case XTENSA_SPECIAL_REG_LCOUNT: return "lcount";
+	case XTENSA_SPECIAL_REG_SAR: return "sar";
+	case XTENSA_SPECIAL_REG_BR: return "br";
+	case XTENSA_SPECIAL_REG_LITBASE: return "litbase";
+	case XTENSA_SPECIAL_REG_SCOMPARE1: return "scompare1";
+	case XTENSA_SPECIAL_REG_ACCLO: return "acclo";
+	case XTENSA_SPECIAL_REG_ACCHI: return "acchi";
+	case XTENSA_SPECIAL_REG_MR0: return "mr0";
+	case XTENSA_SPECIAL_REG_MR1: return "mr1";
+	case XTENSA_SPECIAL_REG_MR2: return "mr2";
+	case XTENSA_SPECIAL_REG_MR3: return "mr3";
+	case XTENSA_SPECIAL_REG_WINDOWBASE: return "windowbase";
+	case XTENSA_SPECIAL_REG_WINDOWSTART: return "windowstart";
+	case XTENSA_SPECIAL_REG_PTEVADDR: return "ptevaddr";
+	case XTENSA_SPECIAL_REG_MMID: return "mmid";
+	case XTENSA_SPECIAL_REG_RASID: return "rasid";
+	case XTENSA_SPECIAL_REG_ITLBCFG: return "itlbcfg";
+	case XTENSA_SPECIAL_REG_DTLBCFG: return "dtlbcfg";
+	case XTENSA_SPECIAL_REG_IBREAKENABLE: return "ibreakenable";
+	case XTENSA_SPECIAL_REG_ATOMCTL: return "atomctl";
+	case XTENSA_SPECIAL_REG_DDR: return "ddr";
+	case XTENSA_SPECIAL_REG_MEPS: return "meps";
+	case XTENSA_SPECIAL_REG_MEPC: return "mepc";
+	case XTENSA_SPECIAL_REG_MESAVE: return "mesave";
+	case XTENSA_SPECIAL_REG_MESR: return "mesr";
+	case XTENSA_SPECIAL_REG_MECR: return "mecr";
+	case XTENSA_SPECIAL_REG_MEVADDR: return "mevaddr";
+	case XTENSA_SPECIAL_REG_IBREAKA0: return "ibreaka0";
+	case XTENSA_SPECIAL_REG_IBREAKA1: return "ibreaka1";
+	case XTENSA_SPECIAL_REG_DEBUGCAUSE: return "debugcause";
+	case XTENSA_SPECIAL_REG_DBREAKA0: return "dbreaka0";
+	case XTENSA_SPECIAL_REG_DBREAKA1: return "dbreaka1";
+	case XTENSA_SPECIAL_REG_DBREAKC0: return "dbreakc0";
+	case XTENSA_SPECIAL_REG_DBREAKC1: return "dbreakc1";
+	case XTENSA_SPECIAL_REG_EPC1: return "epc1";
+	case XTENSA_SPECIAL_REG_EPC2: return "epc2";
+	case XTENSA_SPECIAL_REG_EPC3: return "epc3";
+	case XTENSA_SPECIAL_REG_EPC4: return "epc4";
+	case XTENSA_SPECIAL_REG_EPC5: return "epc5";
+	case XTENSA_SPECIAL_REG_EPC6: return "epc6";
+	case XTENSA_SPECIAL_REG_EPC7: return "epc7";
+	case XTENSA_SPECIAL_REG_DEPC: return "depc";
+	case XTENSA_SPECIAL_REG_EPS2: return "eps2";
+	case XTENSA_SPECIAL_REG_EPS3: return "eps3";
+	case XTENSA_SPECIAL_REG_EPS4: return "eps4";
+	case XTENSA_SPECIAL_REG_EPS5: return "eps5";
+	case XTENSA_SPECIAL_REG_EPS6: return "eps6";
+	case XTENSA_SPECIAL_REG_EPS7: return "eps7";
+	case XTENSA_SPECIAL_REG_EXCSAVE1: return "excsave1";
+	case XTENSA_SPECIAL_REG_EXCSAVE2: return "excsave2";
+	case XTENSA_SPECIAL_REG_EXCSAVE3: return "excsave3";
+	case XTENSA_SPECIAL_REG_EXCSAVE4: return "excsave4";
+	case XTENSA_SPECIAL_REG_EXCSAVE5: return "excsave5";
+	case XTENSA_SPECIAL_REG_EXCSAVE6: return "excsave6";
+	case XTENSA_SPECIAL_REG_EXCSAVE7: return "excsave7";
+	case XTENSA_SPECIAL_REG_CPENABLE: return "cpenable";
+	case XTENSA_SPECIAL_REG_INTERRUPT: return "interrupt";
+	case XTENSA_SPECIAL_REG_INTCLEAR: return "intclear";
+	case XTENSA_SPECIAL_REG_INTENABLE: return "intenable";
+	case XTENSA_SPECIAL_REG_PS: return "ps";
+	case XTENSA_SPECIAL_REG_VECBASE: return "vecbase";
+	case XTENSA_SPECIAL_REG_EXCCAUSE: return "exccause";
+	case XTENSA_SPECIAL_REG_CCOUNT: return "ccount";
+	case XTENSA_SPECIAL_REG_PRID: return "prid";
+	case XTENSA_SPECIAL_REG_ICOUNT: return "icount";
+	case XTENSA_SPECIAL_REG_ICOUNTLEVEL: return "icountlevel";
+	case XTENSA_SPECIAL_REG_EXCVADDR: return "excvaddr";
+	case XTENSA_SPECIAL_REG_CCOMPARE0: return "ccompare0";
+	case XTENSA_SPECIAL_REG_CCOMPARE1: return "ccompare1";
+	case XTENSA_SPECIAL_REG_CCOMPARE2: return "ccompare2";
+	case XTENSA_SPECIAL_REG_MISC0: return "misc0";
+	case XTENSA_SPECIAL_REG_MISC1: return "misc1";
+	case XTENSA_SPECIAL_REG_MISC2: return "misc2";
+	case XTENSA_SPECIAL_REG_MISC3: return "misc3";
+	default: return NULL;
 	}
 	return NULL;
 }
@@ -260,12 +184,9 @@ const char *Xtensa_userreg_name(csh handle, unsigned int id)
 {
 	switch (id)
 	{
-	case XTENSA_USER_REG_THREADPTR:
-		return "THREADPTR";
-	case XTENSA_USER_REG_FCR:
-		return "FCR";
-	case XTENSA_USER_REG_FSR:
-		return "FSR";
+	case XTENSA_USER_REG_THREADPTR: return "threadptr";
+	case XTENSA_USER_REG_FCR: return "fcr";
+	case XTENSA_USER_REG_FSR: return "fsr";
 	default:
 		// TODO: what about the other 252?
 		return NULL;
@@ -300,19 +221,13 @@ const char *Xtensa_kind_name(unsigned int id)
 {
 	switch (id)
 	{
-	case XTENSA_OP_REG:
-		return "Register";
-	case XTENSA_OP_IMM:
-		return "Immediate";
-	case XTENSA_OP_FP:
-		return "Floating-point Register";
-	case XTENSA_OP_SYSREG:
-		return "Special Register";
-	case XTENSA_OP_USERREG:
-		return "User Register";
+	case XTENSA_OP_REG: return "Register";
+	case XTENSA_OP_IMM: return "Immediate";
+	case XTENSA_OP_FP: return "Floating-point Register";
+	case XTENSA_OP_SYSREG: return "Special Register";
+	case XTENSA_OP_USERREG: return "User Register";
 	case XTENSA_OP_INVALID:
 	case XTENSA_OP_MEM:
-	default:
-		return NULL;
+	default: return NULL;
 	}
 }
