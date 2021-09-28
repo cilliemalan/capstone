0380f52ceeda8761549882fef4907f7a02a5b9c7
Cillié Malan <cilliemalan@gmail.com>
2021-09-28 10:32:40 +0200

    Tweaks to Xtensa related notes

--- suite/xtensa/compile_binary.sh
+++ suite/xtensa/compile_binary.sh
@@ -6,6 +6,8 @@
 xtensa-esp32-elf-gcc -c test.S -o test.o
 xtensa-esp32-elf-objdump -d test.o > test.dmp
 xtensa-esp32-elf-objcopy -O binary test.o test.bin
+
+# this code was used to update the xtensa test file
 # CODE=$(hexdump -v -e '16/1 "_x%02X" "\n"' test.bin | sed -E ':a;N;$!ba;s/\n//g; s/_/\\/g; s/\\x  //g; s/.*/"&"/g' | sed 's/\\/\\\\/g')
 # CODE="s/^#define XTENSA_CODE.+/#define XTENSA_CODE $CODE/"
 # sed -i -E "$CODE" ../../tests/test_xtensa.c
